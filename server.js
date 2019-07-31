9e1a3bf4081f3707d8c7e7096be5acd8ee194246
ahrenswett <ahrenswett@gmail.com>
2019-07-30 21:45:14 -0700

    Lab6 day 1 Added location constructor

--- server.js
+++ server.js
@@ -3,26 +3,42 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-const PORT = progress.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('.public'));
+app.use(cors());
+
+function Stuff (request, data){
+    this.searchQuery = request;
+    this.formatedQuery = data.results[0].formatted_address;
+    this.latatude = data.results[0].geometry.location.lat;
+    this.longitude = data.results[0].geometry.location.lng;
+}
+
+
+
+
 
 //return the data locata in the geo.json file
-// app.get('/location', (request, response) => {
-//     try{
-//         let locationData = require('./data/geo.json');
-//         console.log(locationData);
+app.get('/location', (request, response) => {
+    try{
+        let locationData = require('./data/geo.json');
+        // request = 'lynnwood';
+        
 
-//         response.send();
+        let newStuff = new Stuff(request,locationData);
 
-//     } catch(error){
-//         console.log('There was an error!');
-//         response.status(500).send('Server down');
-//     }
+        response.send(newStuff);
+
+    } catch(error){
+        console.log('There was an error!');
+        response.status(500).send('Server down');
+    }
   
-// });
+});
 
 
 //Advise what port we are listening to
-app.listen(PORT, () => console.log('Listening on port ${PORT}'));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
